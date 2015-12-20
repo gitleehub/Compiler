e679e0acea4af0c5de2d6f93a610435a92b22e13
He <szhgoldeneye@hotmail.com>
2015-12-20 19:31:40 +0800

    变量申明及错误处理 四则运算的三地址输出

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -97,6 +97,7 @@ public class Node {
 		this.type = type;
 	}
 
+
 	/**
 	 * 获取该结点的层级
 	 *
@@ -120,12 +121,7 @@ public class Node {
 	 *
 	 * @return 所有孩子结点
 	 */
-	public List<Node> getChilds() {
-		if (this.hasChild())
-			return childs;
-		else
-			return null;
-	}
+	public List<Node> getChilds() {	return childs;	}
 
 	/**
 	 * 检查是否存在孩子
